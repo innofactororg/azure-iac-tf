c6faa131cefb5f1cd9d07d63e46dfd082fe649e4
jorseng <34156375+jorseng@users.noreply.github.com>
2022-08-24 10:26:00 +0000

    remove admin condition

--- modules/databases/mssql_server/server.tf
+++ modules/databases/mssql_server/server.tf
@@ -12,13 +12,13 @@ resource "azurerm_mssql_server" "mssql" {
 
 
   dynamic "azuread_administrator" {
-    for_each = can(var.settings.azuread_administrator) ? [var.settings.azuread_administrator] : []
+    for_each = try(var.settings.azuread_administrator, {}) == {} ? [] : [1]
 
     content {
       azuread_authentication_only = try(var.settings.azuread_administrator.azuread_authentication_only, false)
-      login_username              = try(var.settings.azuread_administrator.login_username, try(var.azuread_groups[var.client_config.landingzone_key][var.settings.azuread_administrator.azuread_group_key].name, var.azuread_groups[var.settings.azuread_administrator.lz_key][var.settings.azuread_administrator.azuread_group_key].name))
-      object_id                   = try(var.settings.azuread_administrator.object_id, try(var.azuread_groups[var.client_config.landingzone_key][var.settings.azuread_administrator.azuread_group_key].id, var.azuread_groups[var.settings.azuread_administrator.lz_key][var.settings.azuread_administrator.azuread_group_key].id))
-      tenant_id                   = try(var.settings.azuread_administrator.tenant_id, try(var.azuread_groups[var.client_config.landingzone_key][var.settings.azuread_administrator.azuread_group_key].tenant_id, var.azuread_groups[var.settings.azuread_administrator.lz_key][var.settings.azuread_administrator.azuread_group_key].tenant_id))
+      login_username = try(var.settings.azuread_administrator.login_username, try(var.azuread_groups[var.client_config.landingzone_key][var.settings.azuread_administrator.azuread_group_key].name, var.azuread_groups[var.settings.azuread_administrator.lz_key][var.settings.azuread_administrator.azuread_group_key].name))
+      object_id      = try(var.settings.azuread_administrator.object_id, try(var.azuread_groups[var.client_config.landingzone_key][var.settings.azuread_administrator.azuread_group_key].id, var.azuread_groups[var.settings.azuread_administrator.lz_key][var.settings.azuread_administrator.azuread_group_key].id))
+      tenant_id      = try(var.settings.azuread_administrator.tenant_id, try(var.azuread_groups[var.client_config.landingzone_key][var.settings.azuread_administrator.azuread_group_key].tenant_id, var.azuread_groups[var.settings.azuread_administrator.lz_key][var.settings.azuread_administrator.azuread_group_key].tenant_id))
     }
   }
 
