67047ea705aa8de4bbb8b9bc61023c72a840e737
Rohit Alaraddi <rohitalaraddi1996@gmail.com>
2023-02-27 13:03:15 +0100

    changed: for loop

--- modules/security/sentinel/automation_rule/module.tf
+++ modules/security/sentinel/automation_rule/module.tf
@@ -1,66 +1,41 @@
-resource "azurerm_sentinel_alert_rule_scheduled" "scheduled" {
+resource "azurerm_sentinel_automation_rule" "automation_rule" {
   name                       = var.name
-  log_analytics_workspace_id = lower(var.log_analytics_workspace_id)
+  log_analytics_workspace_id = var.log_analytics_workspace_id
   display_name               = var.display_name
-  severity                   = var.severity
-  query                      = var.query
-  alert_rule_template_guid   = var.alert_rule_template_guid
-  description                = var.description
+  order                      = var.order
   enabled                    = var.enabled
-  query_frequency            = var.query_frequency
-  query_period               = var.query_period
-  suppression_duration       = var.suppression_duration
-  suppression_enabled        = var.suppression_enabled
-  tactics                    = var.tactics
-  trigger_operator           = var.trigger_operator
-  trigger_threshold          = var.trigger_threshold
+  expiration                 = var.expiration
 
-  dynamic "event_grouping" {
-    for_each = lookup(var.settings, "event_grouping", {}) != {} ? [1] : []
-
+  dynamic "action_incident" {
+    for_each = var.modify_properties== null ? [] : var.modify_properties
     content {
-      aggregation_method = lookup(var.settings.event_grouping, "aggregation_method", null)
+      order                  = try(action_incident.value.order, null)
+      status                 = try(action_incident.value.action_incident.status, null)
+      classification         = try(action_incident.value.classification, null)
+      classification_comment = try(action_incident.value.classificationComment, null)
+      labels                 = try(action_incident.value.labels, null)
+      owner_id               = try(action_incident.value.owner, null)
+      severity               = try(action_incident.value.severity, null)
     }
   }
 
-  dynamic "incident_configuration" {
-    for_each = lookup(var.settings, "incident_configuration", {}) != {} ? [1] : []
-
+  dynamic "action_playbook" {
+    for_each = var.run_playbook == null ? [] : var.run_playbook
     content {
-      create_incident = lookup(var.settings.incident_configuration, "create_incident", null)
-
-      dynamic "grouping" {
-        for_each = lookup(var.settings.incident_configuration, "grouping", {}) != {} ? [1] : []
-
-        content {
-          enabled                 = lookup(var.settings.incident_configuration.grouping, "enabled", true)
-          lookback_duration       = lookup(var.settings.incident_configuration.grouping, "lookback_duration", "PT5M")
-          reopen_closed_incidents = lookup(var.settings.incident_configuration.grouping, "reopen_closed_incidents", false)
-          entity_matching_method  = lookup(var.settings.incident_configuration.grouping, "entity_matching_method", null)
-          group_by_entities       = lookup(var.settings.incident_configuration.grouping, "group_by_entities", null)
-          group_by_alert_details  = lookup(var.settings.incident_configuration.grouping, "group_by_alert_details", null)
-          group_by_custom_details = lookup(var.settings.incident_configuration.grouping, "group_by_custom_details", null)
-        }
-      }
+      logic_app_id   = action_playbook.value.logicAppResourceId
+      order  = action_playbook.value.order
+      tenant_id = action_playbook.value.tenantId
     }
   }
 
-  alert_details_override {
-    display_name_format = var.display_name_format
-    description_format = null
-    tactics_column_name = null
-    severity_column_name = null
-  }
-
-  dynamic "entity_mapping" {
-    for_each = var.entity_mappings == null ? [] : var.entity_mappings
+  dynamic "condition" {
+    for_each = var.condition_type == null ? [] : var.condition_type
     content {
-      entity_type = entity_mapping.value.entityType
-      field_mapping {
-        identifier = entity_mapping.value.fieldMappings[0].identifier
-        column_name = entity_mapping.value.fieldMappings[0].columnName
-      }
+      operator = try(condition.value.conditionProperties.operator, null)
+      property = try(condition.value.conditionProperties.propertyName, null)
+      values   = try(condition.value.conditionProperties.propertyValues, null)
     }
   }
+  
 
 }
