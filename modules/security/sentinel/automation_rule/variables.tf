4f751e95c9968df05231ed1cb76d451adaba7d9c
Rohit Alaraddi <rohitalaraddi1996@gmail.com>
2023-02-27 11:44:31 +0100

    added : changes related to automation rules

--- modules/security/sentinel/automation_rule/variables.tf
+++ modules/security/sentinel/automation_rule/variables.tf
@@ -1,43 +1,62 @@
 variable "name" {
   default = null
 }
-variable "display_name" {
-  default = null
-}
-variable "settings" {
-  default = null
-}
 variable "log_analytics_workspace_id" {
   default = null
 }
-variable "order" {
+variable "display_name" {
+  default = null
+}
+variable "severity" {
+  default = null
+  description = "values: High, Medium, Low, Informational"
+}
+variable "query" {
+  default = null
+}
+variable "alert_rule_template_guid" {
+  default = null
+}
+variable "description" {
   default = null
 }
 variable "enabled" {
   default = null
 }
-variable "expiration" {
+variable "query_frequency" {
   default = null
 }
-variable "combined_objects_logic_app_workflow" {
+variable "query_period" {
   default = null
 }
-variable "client_config" {
+variable "suppression_duration" {
   default = null
 }
-variable "action_order" {
+variable "suppression_enabled" {
   default = null
 }
-variable "condition_type" {
+variable "tactics" {
   default = null
 }
-variable "modify_properties" {
+variable "trigger_operator" {
+  default = null
+}
+variable "trigger_threshold" {
+  default = null
+}
+variable "settings" {
   default = null
 }
 
-variable "run_playbook" {
+variable "definition_file" {
+  default = null
+  description = "YAML File from which to load the definition, based on the schema defined in the Sentinel API and published "
+}
+
+variable "display_name_format" {
   default = null
 }
 
-
-
+variable "entity_mappings" {
+  default = null
+}
