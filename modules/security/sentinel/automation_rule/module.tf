0a31376d3b6b4f8c1b00df7f763c69c8909553ad
Rohit Alaraddi <rohitalaraddi1996@gmail.com>
2023-03-01 10:38:41 +0100

    changed the logic

--- modules/security/sentinel/automation_rule/module.tf
+++ modules/security/sentinel/automation_rule/module.tf
@@ -20,72 +20,71 @@ resource "azurerm_sentinel_automation_rule" "automation_rule" {
     }
   }
 
-  dynamic "action_playbook" {
-  for_each = [for action in var.action_order : {
-    order             = action.action_order
-    action_type       = action.actionType
-    logic_app_resource_id = action.actionConfiguration.logicAppResourceId
-    tenant_id         = action.actionConfiguration.tenantId
-  }]
-
-  content {
-    order = action_playbook.value.order
-
-
-    # actionConfiguration = {
-      logic_app_id  = action_playbook.value.logic_app_resource_id
-      tenant_id         = action_playbook.value.tenant_id
-    # }
-  }
-  }
-  
-  dynamic "condition" {
-  for_each = [for condition in var.condition_type : {
-    type        = condition.condition_type
-    property    = condition.conditionProperties.propertyName
-    operator    = condition.conditionProperties.operator
-    values      = condition.conditionProperties.propertyValues
-  }]
-
-  content {
-    # type = condition.value.type
-
-    # conditionProperties = {
-      property = condition.value.property
-      operator = condition.value.operator
-      values   = condition.value.values
-    # }
-  }
-  }
-
-
   # dynamic "action_playbook" {
-  #   for_each = var.action_order == null ? [] : var.action_order
+  # for_each = [for action in var.action_order : {
+  #   order             = action.action_order
+  #   action_type       = action.actionType
+  #   logic_app_resource_id = action.actionConfiguration.logicAppResourceId
+  #   tenant_id         = action.actionConfiguration.tenantId
+  # }]
 
-  #   content {
-  #     order        = try(action_playbook.value.action_order, null)
-      
-  #       # actionConfiguration {
-  #           logic_app_id = try(action_playbook.value.actionConfiguration.logicAppResourceId, null)
-  #           tenant_id    = try(action_playbook.value.actionConfiguration.tenantId, null)
-  #       # }
+  # content {
+  #   order = action_playbook.value.order
 
-  #   }
+
+  #   # actionConfiguration = {
+  #     logic_app_id  = action_playbook.value.logic_app_resource_id
+  #     tenant_id         = action_playbook.value.tenant_id
+  #   # }
   # }
-
+  # }
+  
   # dynamic "condition" {
-  #   for_each = var.condition_type == null ? [] : var.condition_type
+  # for_each = [for condition in var.condition_type : {
+  #   type        = condition.condition_type
+  #   property    = condition.conditionProperties.propertyName
+  #   operator    = condition.conditionProperties.operator
+  #   values      = condition.conditionProperties.propertyValues
+  # }]
 
-  #   content {
+  # content {
+  #   # type = condition.value.type
 
-  #     # conditionType = try(action_playbook.value.condition_type, null)
-
-  #     # conditionProperties {
-  #         operator = try(condition.value.conditionProperties[0].operator, null)
-  #         property = try(condition.value.conditionProperties[0].propertyName, null)
-  #         values   = try(condition.value.conditionProperties[0].propertyValues, null)
-  #         # }
-  #   }
+  #   # conditionProperties = {
+  #     property = condition.value.property
+  #     operator = condition.value.operator
+  #     values   = condition.value.values
+  #   # }
   # }
+  # }
+
+  dynamic "action_playbook" {
+    for_each = var.action_order == null ? [] : var.action_order
+
+    content {
+      order        = try(action_playbook.value.order, null)
+      
+        # actionConfiguration {
+            logic_app_id = try(action_playbook.value.actionConfiguration.logicAppResourceId, null)
+            tenant_id    = try(action_playbook.value.actionConfiguration.tenantId, null)
+        # }
+
+    }
+  }
+
+  dynamic "condition" {
+    for_each = var.condition_type == null ? [] : var.condition_type
+
+    content {
+
+      # conditionType = try(action_playbook.value.condition_type, null)
+
+      # conditionProperties {
+          operator = try(condition.value.conditionProperties[0].operator, null)
+          property = try(condition.value.conditionProperties[0].propertyName, null)
+          values   = try(condition.value.conditionProperties[0].propertyValues, null)
+          # }
+    }
+  }
 
 }
