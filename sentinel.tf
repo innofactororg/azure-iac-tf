c150850e88543bb46eaa8634aae18cad880f91de
Rohit Alaraddi <rohitalaraddi1996@gmail.com>
2023-03-01 10:03:53 +0100

    changed the variable value

--- sentinel.tf
+++ sentinel.tf
@@ -11,8 +11,8 @@ module "sentinel_automation_rules" {
   expiration                          = try(each.value.expiration, yamldecode(file("${path.cwd}/${each.value.definition_file}"))["properties"]["triggeringLogic"].expiration, null)
   combined_objects_logic_app_workflow = local.combined_objects_logic_app_workflow
   client_config                       = local.client_config
-  action_order                        = try(each.value.action_order, yamldecode(file("${path.cwd}/${each.value.definition_file}"))["properties"]["actions"].order, null)
-  condition_type                      = try(each.value.condition_type, yamldecode(file("${path.cwd}/${each.value.definition_file}"))["properties"]["conditions"], null)
+  action_order                        = try(each.value.action_order, yamldecode(file("${path.cwd}/${each.value.definition_file}"))["properties"]["actions"], null)
+  condition_type                      = try(each.value.condition_type, yamldecode(file("${path.cwd}/${each.value.definition_file}"))["properties"]["triggeringLogic"]["conditions"], null)
 }
 
 module "sentinel_watchlists" {
