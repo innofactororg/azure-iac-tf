6bbe29a98593339bf085086785c61e6993556308
Rohit Alaraddi <rohitalaraddi1996@gmail.com>
2023-03-07 10:37:38 +0100

    changed the logic

--- modules/security/sentinel/automation_rule/module.tf
+++ modules/security/sentinel/automation_rule/module.tf
@@ -7,12 +7,12 @@ resource "azurerm_sentinel_automation_rule" "automation_rule" {
   expiration                 = var.expiration
 
   dynamic "action_incident" {
-    for_each = var.action_type == "RunPlaybook" ? [] : var.action_type
+    for_each = var.order == null ? [] : var.action_order
     # for_each = lookup(var.action_type, "RunPlaybook", {}) != {} ? [1] : [] 
 
     content {
-      order                  = try(action_incident.value.order, null)
-      status                 = try(action_incident.value.actionConfiguration.status, null)
+      order                  = try(action_incident.value.order, 1)
+      status                 = try(action_incident.value.actionConfiguration.status, "New")
       classification         = try(action_incident.value.actionConfiguration.classification, null)
       classification_comment = try(action_incident.value.actionConfiguration.classificationComment, null)
       labels                 = try(action_incident.value.actionConfiguration.labels, null)
@@ -21,46 +21,8 @@ resource "azurerm_sentinel_automation_rule" "automation_rule" {
     }
   }
 
-  # dynamic "action_playbook" {
-  # for_each = [for action in var.action_order : {
-  #   order             = action.action_order
-  #   action_type       = action.actionType
-  #   logic_app_resource_id = action.actionConfiguration.logicAppResourceId
-  #   tenant_id         = action.actionConfiguration.tenantId
-  # }]
-
-  # content {
-  #   order = action_playbook.value.order
-
-
-  #   # actionConfiguration = {
-  #     logic_app_id  = action_playbook.value.logic_app_resource_id
-  #     tenant_id         = action_playbook.value.tenant_id
-  #   # }
-  # }
-  # }
-  
-  # dynamic "condition" {
-  # for_each = [for condition in var.condition_type : {
-  #   type        = condition.condition_type
-  #   property    = condition.conditionProperties.propertyName
-  #   operator    = condition.conditionProperties.operator
-  #   values      = condition.conditionProperties.propertyValues
-  # }]
-
-  # content {
-  #   # type = condition.value.type
-
-  #   # conditionProperties = {
-  #     property = condition.value.property
-  #     operator = condition.value.operator
-  #     values   = condition.value.values
-  #   # }
-  # }
-  # }
-
   dynamic "action_playbook" {
-    for_each = var.action_type == "ModifyProperties" ? [] : var.action_type
+    for_each = var.order == null ? [] : var.action_order
 
     content {
       order        = try(action_playbook.value.order, null)
