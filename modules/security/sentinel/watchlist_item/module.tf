f0e6f7930d310b6d347c9d7c3ca5d46e036a9b4d
Rohit Alaraddi <rohitalaraddi1996@gmail.com>
2023-03-13 12:42:36 +0100

    uncommenting name

--- modules/security/sentinel/watchlist_item/module.tf
+++ modules/security/sentinel/watchlist_item/module.tf
@@ -2,7 +2,7 @@ resource "azurerm_sentinel_watchlist_item" "watchlist_item" {
 
   for_each   = { for idx, value_watchlist in var.properties: idx => idx}
 
-  name         = var.name
+  # name         = var.name
   watchlist_id = var.watchlist_id
   properties   = var.properties[each.key]
 }
