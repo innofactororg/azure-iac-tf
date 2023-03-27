977eed80aad3388b72447052d597064f41e9027e
martinjensen225 <martin.jensen@innofactor.com>
2023-03-27 17:43:07 +0200

    fix: dsc extension revert testing config

--- modules/compute/virtual_machine_extensions/wvd_dsc.tf
+++ modules/compute/virtual_machine_extensions/wvd_dsc.tf
@@ -18,7 +18,7 @@ resource "azurerm_virtual_machine_extension" "session_host_dscextension" {
           try(var.extension.host_pool_name, ""),
           try(var.wvd_host_pools[var.extension.host_pool.lz_key][var.extension.host_pool.host_pool_key].name, var.wvd_host_pools[var.client_config.landingzone_key][var.extension.host_pool.host_pool_key].name)
         ),
-        "aadJoin": true
+        # "aadJoin": true
       }
     }
   )
