aztfmod/terraform-azurerm-caf

add support for application_insights_web_test

examples/variables.tf
@@ -309,6 +309,9 @@ variable "synapse_workspaces" {
 variable "azurerm_application_insights" {
   default = {}
 }
+variable "azurerm_application_insights_web_test" {
+  default = {}
+}
 variable "role_mapping" {
   default = {}
 }
@@ -1025,10 +1028,10 @@ variable "iot_dps_shared_access_policy" {
 }
 variable "iot_hub_consumer_groups" {
   default = {}
-} 
+}
 variable "iot_hub_certificate" {
   default = {}
-} 
+}
 variable "resource_provider_registration" {
   default = {}
 }

innofactororg/azure-iac-tf

examples/variables.tf
@@ -1017,3 +1017,12 @@ variable "static_sites" {
 variable "aro_clusters" {
   default = {}
 }
+variable "web_pubsubs" {
+  default = {}
+}
+variable "web_pubsub_hubs" {
+  default = {}
+}
+variable "aadb2c_directory" {
+  default = {}
+}