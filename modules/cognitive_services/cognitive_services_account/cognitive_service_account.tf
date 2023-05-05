d997a5b2b87611a9ac5048ef12cb03a5711b34d4
Damian Flynn <info@damianflynn.com>
2023-05-05 23:15:51 +0100

    fix: azurerm_cognitive_account

--- modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
+++ modules/cognitive_services/cognitive_services_account/cognitive_service_account.tf
@@ -22,7 +22,10 @@ resource "azurerm_cognitive_account" "service" {
     content {
       default_action             = var.settings.network_acls.default_action
       ip_rules                   = try(var.settings.network_acls.ip_rules, null)
-      virtual_network_subnet_ids = try(var.settings.network_acls.virtual_network_subnet_ids, null)
+      virtual_network_rules {
+        subnet_id = try(var.settings.network_acls.virtual_network_rules.subnet_id, null)
+        ignore_missing_vnet_service_endpoint = false
+      }
     }
   }
 
